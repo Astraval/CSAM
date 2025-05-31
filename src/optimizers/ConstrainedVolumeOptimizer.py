2ea61fc62a1b34fb4a632e78ebadd5d1141511ec
Astraval <fasterlingpierre@gmail.com>
2025-05-31 15:13:30 +0200

    HypercubeOptimizer added

--- src/optimizers/ConstrainedVolumeOptimizer.py
+++ src/optimizers/ConstrainedVolumeOptimizer.py
@@ -1,3 +1,4 @@
+import copy
 from abc import ABC, abstractmethod
 
 import torch.nn
@@ -7,22 +8,33 @@ from src.cert import Safebox
 
 
 class ConstrainedVolumeOptimizer(ABC):
-    def __init__(self, model: torch.nn.Sequential):
-        self._interval_model = Safebox.modelToBModel(model)
+    def __init__(self, model: torch.nn.Sequential, quiet: bool = False):
+        self._current_volume = None
+        self._interval_model: torch.nn.Sequential = Safebox.modelToBModel(model)
+        self._quiet = quiet
 
     @abstractmethod
-    def set_volume_constrain(self, volume: float):
+    def _set_volume_constrain(self, volume: float):
         raise NotImplementedError
+
+    def set_volume_constrain(self, volume: float):
+        self._current_volume = volume
+
+    def _print(self, *messages, **kwargs):
+        if not self._quiet:
+            print(*messages, **kwargs)
 
     def train(self,
               train_dataset: torch.utils.data.Dataset,
               val_dataset: torch.utils.data.Dataset,
-              loss_obj: float, max_iters: int = 100, batch_size: int = 64, lr: float = 1e-4, **kwargs):
+              loss_obj: float, max_iters: int = 100,
+              batch_size: int = 64, lr: float = 1e-4, **kwargs) -> torch.nn.Sequential:
         self._interval_model.train()
         dataloader = torch.utils.data.DataLoader(train_dataset, batch_size == batch_size, shuffle=True)
         data_iter = iter(dataloader)
-        progress_bar = tqdm(range(max_iters))
-        for i in progress_bar:
+        progress_bar = tqdm(range(max_iters), disable=self._quiet)
+        loss = 0
+        for _ in progress_bar:
             X, y = next(data_iter, (None, None))
             if X is None:
                 data_iter = iter(dataloader)
@@ -30,8 +42,17 @@ class ConstrainedVolumeOptimizer(ABC):
             loss = self.step(X, y, lr=lr, **kwargs)
             progress_bar.set_postfix({
                 "loss": round(loss, 4),
-                "min_val_acc": self._evaluate_min_val_acc(val_dataset)
+                "min_val_acc": round(self._evaluate_min_val_acc(val_dataset), 4)
             })
+            self._interval_model.train()
+            if loss < loss_obj:
+                self._print("Loss objective reached. Stop training.")
+                break
+        self._print("-" * 10,
+                    f" Training with volume parameter constrain {round(self._current_volume, 4)} completed with loss  "
+                    f"{round(loss)}",
+                    "-" * 10)
+        return copy.deepcopy(self._interval_model)
 
     def _evaluate_min_val_acc(self,
                               val_dataset: torch.utils.data,
@@ -41,8 +62,8 @@ class ConstrainedVolumeOptimizer(ABC):
         with torch.no_grad():
             X = X.unsqueeze(-1)
             X = X.expand(*X.shape[:-1], 2)
-            preds = self._interval_model(X)
-            min_acc = Safebox.min_acc(y, preds)
+            y_pred = self._interval_model(X)
+            min_acc = Safebox.min_acc(y, y_pred)
         return min_acc
 
     @abstractmethod
--- src/optimizers/HypercubeOptimizer.py
+++ src/optimizers/HypercubeOptimizer.py
@@ -7,9 +7,40 @@ from src.optimizers.ConstrainedVolumeOptimizer import ConstrainedVolumeOptimizer
 class HypercubeOptimizer(ConstrainedVolumeOptimizer):
     def __init__(self, model: torch.nn.Sequential):
         super().__init__(model)
+        self._optimizer = None
 
-    def set_volume_constrain(self, volume: float):
+    def _set_volume_constrain(self, volume: float):
         Safebox.assign_epsilon(self._interval_model, volume)
 
+    def train(self,
+              train_dataset: torch.utils.data.Dataset,
+              val_dataset: torch.utils.data.Dataset,
+              loss_obj: float, max_iters: int = 100, batch_size: int = 64, lr: float = 1e-4, **kwargs):
+        for layer in self._interval_model:
+            if not isinstance(layer, Safebox.BFlatten):
+                layer.W_c.requires_grad = True  # only enable to center updates
+                layer.b_c.requires_grad = True
+                layer.W_u.requires_grad = False
+                layer.b_u.requires_grad = False
+                layer.W_l.requires_grad = False
+                layer.b_l.requires_grad = False
+
+        self._optimizer = torch.optim.Adam(
+            filter(lambda param: param.require_grad, self._interval_model.parameters()), lr=lr
+        )  # only chose centers in optimizer
+        self._optimizer.zero_grad()
+        super().train(
+            train_dataset, val_dataset, loss_obj, max_iters=max_iters, batch_size=batch_size, lr=lr, **kwargs
+        )
+
     def step(self, X: torch.Tensor, y: torch.Tensor, lr: float = 1e-4, **kwargs) -> float:
-        pass
+        self._optimizer.zero_grad()
+        self._interval_model.zero_grad()
+        self._interval_model.train()
+        X = X.unsqueeze(-1)
+        X = X.expand(*X.shape[:-1], 2)
+        y_pred = self._interval_model(X)
+        max_loss = Safebox.max_loss(y, y_pred)
+        max_loss.backward()
+        self._optimizer.step()
+        return max_loss.item()
