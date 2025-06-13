c92cdf2f6307bd3db680d5c95856e56f09f3de47
Astraval <fasterlingpierre@gmail.com>
2025-06-13 15:48:19 +0200

    Safebox disclaimer

--- src/cert/Safebox.py
+++ src/cert/Safebox.py
@@ -1,3 +1,10 @@
+"""
+This code has been provided by Professor Matthew Wicker at Imperial College London.
+We conducted the experiments using this code for our IBP.
+We have Professor Wicker's official authorization to use his code.
+All rights reserved.
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
